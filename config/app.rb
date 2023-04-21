bc8e9cafb53ed5a0eb67f670e23deb28abb2230a
Chi Man Lei <chimanlei@gmail.com>
2023-04-21 11:25:09 +0800

    minor code refactored

--- config/app.rb
+++ config/app.rb
@@ -1,11 +1,10 @@
 # frozen_string_literal: true
 
 require "hanami"
+require_relative "version"
 
 module Certman
   class App < Hanami::App
-    VERSION = "0.1.0"
-
     config.actions.format :json
     config.middleware.use :body_parser, :json
   end
